63f654094e2acccaf6662fe3a3accbcae95de82b
Annalisa <annalisa.bauer1@gmail.com>
2021-09-16 15:32:34 -0700

    completed readme generator. Need to add badges

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# readme-generator
+# asldfa
+
+## Description
+asdfa
+
+## Technology
+asdf
+
+## Table of Contents
+asd
+
+## Installation
+asdf
+
+## Usage
+asdf
+
+## License
+MIT,Artistic,Apache,CC0,WTFPL
+
+## Contribution
+
+
+## Tests
+asdf
--- index.js
+++ index.js
@@ -1,13 +1,90 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const generateMD = (questions) =>
+  `# ${questions.title}
+
+## Description
+${questions.description}
+
+## Technology
+${questions.technology}
+
+## Table of Contents
+${questions.contents}
+
+## Installation
+${questions.installation}
+
+## Usage
+${questions.usage}
+
+## License
+${questions.license}
+
+## Contribution
+${questions.contribution}
+
+## Tests
+${questions.tests}`;
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Provide a description of your project.",
+    },
+    {
+      type: "input",
+      name: "technology",
+      message: "What are the technologies in your project?",
+    },
+    {
+      type: "input",
+      name: "contents",
+      message: "Provide a list of table of contents.",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "How do we install your dependencies?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "What is the usage for this code?",
+    },
+    {
+      type: "checkbox",
+      name: "license",
+      message: "What is the license for this project?",
+      choices: ["MIT", "Artistic", "Apache", "CC0", "WTFPL"],
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: "How can other people contribute?",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "What tests did we preform in this project?",
+    },
+  ])
+  .then((questions) => {
+    const mdPageContent = generateMD(questions);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    fs.writeFile("readme.md", mdPageContent, (err) =>
+      err ? console.log(err) : console.log("Successfully created readme.md!")
+    );
+  });
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
-init();
